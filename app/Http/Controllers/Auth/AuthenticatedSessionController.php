bb46d9d8bc14353fe53d80974ee26764a4a716ba
josep562 <61271631+josep562@users.noreply.github.com>
2024-10-16 17:29:12 +0300

    Update AuthenticatedSessionController.php
    Added absolute path

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -17,7 +17,7 @@ class AuthenticatedSessionController extends Controller
      */
     public function create(): View
     {
-        return view('auth.login');
+        return view('/resources/views/auth/login.blade.php');
     }
 
     /**
